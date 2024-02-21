b0ce70b5c08eedea1665424ecf3d17fcc8161e25
Abdelmoneim000 <ab000arabi22@gmail.com>
2024-02-21 04:56:51 +0200

    Finish the (Select Course) section and add a Course card component

---
+++ src/components/Course.svelte
@@ -0,0 +1,60 @@
+<!-- Course card component to display for each course -->
+<script>
+  export let course = "";
+  export let difficulty = "";
+  export let description = "";
+  let clicked = false;
+
+    const handleClick = () => {
+        clicked = !clicked;
+    };
+</script>
+
+
+<style>
+    .card {
+        font-family: "Inter";
+        width: 20%;
+        border: 3px solid black;
+        border-radius: 20px;
+        padding: 30px;
+        margin: 10px;
+        cursor: pointer;
+    }
+    .title {
+        display: flex;
+        justify-content: space-between;
+    }
+
+    .title div {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        width: calc(100% - 190px);
+        height: 40px;
+        margin-top: 10px;
+        background-color: #EBEBF0;
+        border-radius: 16px;
+    }
+    .description {
+        margin-top: 10px;
+    }
+
+    .description p {
+        font-size: 32px;
+        line-height: 40px;
+    }
+</style>
+
+
+<div class="card" style="background-color: {clicked ? '#ECECEC' : 'var(--background-color)'};" on:click={handleClick}>
+    <div class="title">
+        <div style="border: {clicked ? '2px solid black' : 'none'};">
+            <h3>{course}</h3>
+        </div>
+        <h3 style="color: #449108;">{difficulty}</h3>
+    </div>
+    <div class="description">
+        <p>{description}</p>
+    </div>
+</div>
---
+++ src/routes/dashboard/+page.svelte
@@ -0,0 +1,64 @@
+<!-- Dashboard should only be viewed when the user logs in -->
+<script>
+    import Course from "../../components/Course.svelte";
+    import Button from "../../components/button.svelte";
+
+    let selectedCourse = "";
+</script>
+
+<style>
+    h1 {
+        font-family: "Inter";
+        font-size: 48px;
+        margin: 20px;
+        text-align: center;
+    }
+
+    .card_view {
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: center;
+        gap: 50px;
+        margin-top: 3em;
+    }
+
+    .card-container {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+    }
+
+    .card-container > div:last-child {
+        position: relative;
+        left: 23.5%;
+        margin-top: 3em;
+    }
+</style>
+
+<h1>Choose a course to enroll in.</h1>
+<div class="card-container">
+    <div class="card_view">
+        <Course
+        course="Python"
+        difficulty="Easy"
+        description="Python is a user friendly, high-level programming language popular for its readability"
+        />
+        <Course
+        course="Web Trio"
+        difficulty="Easy"
+        description="HTML, CSS, and JavaScript are the building blocks of the web"
+        />
+    </div>
+    <div>
+        <Button
+        backgroundColor="#FFE37F"
+        width="100px"
+        border="1.2px solid black"
+        borderRadius="9.6px"
+        text="Next" />
+    </div>
+</div>
+
+
+
+
